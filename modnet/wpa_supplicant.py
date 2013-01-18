b5382a4f8048926e620f0b80ba8a677ea516b5f1
Josh Driver <keeperofdakeys@gmail.com>
2013-01-18 23:28:52 +1030

    Initial wpa_supplicant connection skeleton.

---
+++ modnet/wpa_supplicant.py
@@ -0,0 +1,23 @@
+from connections import WirelessConnection
+
+class WpaSupplicant():
+  def __init__(self):
+    pass
+
+  def get_status(self):
+    pass
+
+  def connect(self, WirelessConnection):
+    pass
+
+  def disconnect(self):
+    pass
+
+  def reassociate(self):
+    pass
+
+  def scan(self):
+    pass
+
+  def roma(self):
+    pass
