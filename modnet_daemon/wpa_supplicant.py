6360a7f63d7059d41618a6ef5872d4d7de680446
Josh Driver <keeperofdakeys@gmail.com>
2013-01-22 01:01:04 +1030

    Adding actual template code for process control in modnet_daemon.

--- modnet_daemon/wpa_supplicant.py
+++ modnet_daemon/wpa_supplicant.py
@@ -1,12 +1,30 @@
+import os, signal
+
 class WpaSupplicant():
-  def __init__(self, path):
-    pass
+  def __init__(self, pid_file, config_file, prog_path, proc_name, interface):
+    self.program = prog_path
+    self.process_name = proc_name
+    self.prog_args = [self.program,
+        "-c %s" % config_file, "-B",
+        "-P %s" % pid_file, 
+        "-Dnl80211",
+        "-i %s" % interface]
+    self.started = false
 
   def start(self):
-    pass
+    if self.running == True:
+      return
+    os.spawnv(os.P_WAIT, self.program, self.prog_args)
+    self.pid = 0
+    self.running = True
 
   def stop(self):
-    pass
+    if self.running == False:
+      return
+    # test if PID points to the correct process
+    os.kill(self.pid, signal.SIGTERM)
 
   def restart(self):
+    stop(self)
+    start(self)
     pass
