f7846c60ce43e39be943f7a50b24cd4b6132ae82
Josh Driver <keeperofdakeys@gmail.com>
2013-01-28 20:36:59 +1100

    Defining modnet_daemon interface.

---
+++ modnet/modnet_daemon.py
@@ -0,0 +1,18 @@
+class ModnetDaemon:
+  def __init__(self):
+    pass
+
+  def start_wpa_supplicant(self):
+    pass
+
+  def start_dhcpcd(self, interface_name): # STUB: need to add options
+    pass
+
+  def start_dhclient(self, interface_name): # STUB: need to add options
+    pass
+
+  def stop_dhcpcd(self, interface_name):
+    pass
+
+  def stop_dhclient(self, interface_name):
+    pass
