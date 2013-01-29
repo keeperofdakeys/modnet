9729abc459888226ce9284fe330d1a7a4012ae57
Josh Driver <keeperofdakeys@gmail.com>
2013-01-29 18:27:09 +1100

    Adding design, adding configuration stuff.

--- modnet/modnet.py
+++ modnet/modnet.py
@@ -1,8 +1,18 @@
-import Configuration
+import configparser
 
 def main():
-  connection_config = ConfigurationFile("~/.config/modnet/connections.py")
-  connections = connection_config.load_config()
+  configuration = {}
+
+  for config_name in {'general', 'interfaces', 'connections', 'wireless_connections'}:
+    config_file = config_dir + config_name + '.conf'
+    if os.path.exists(config_file):
+      config = configparser.ConfigParser()
+      config.read(config_file)
+      configuration[config_name] = config
+    else:
+      configuration[config_name] = {}
+
+
 
   # make interfaces
 
