2484476b791129a512f51264b578662aee398865
Josh Driver <keeperofdakeys@gmail.com>
2013-02-09 02:06:13 +1030

    Modifying things.

--- modnet/dbus.py
+++ modnet/dbus.py
@@ -1,11 +1,12 @@
+import threading
+
 class Dbus_Thread(threading.Thread):
-  def __init__(self, dbus_outgoing_queue, dbus_signal_queue):
-    self.outgoing_queue = dbus_outgoing_queue
-    self.signal_queue = dbus_signal_queue
+  def __init__(self, msg_queue):
+    self.msg_queue = msg_queue
 
   def run(self):
     while True:
-      item = self.outgoing_queue.get()
+      item = self.msg_queue.get()
       handle_msg(item)
 
   def handle_msg(item):
--- modnet/modnet.py
+++ modnet/modnet.py
@@ -1,54 +1,66 @@
 import configparser
 import interfaces
+import connections
+import Queue
 
-def main():
-  configuration = {}
-  config_dir = "/home/blueteeth/.config/modnet/"
+class Modnet:
+  def __init__(self):
+    configuration = {}
+    config_dir = "/home/blueteeth/.config/modnet/"
 
-  for config_name in {'general', 'interfaces', 'connections', 'wireless_connections'}:
-    config_file = config_dir + config_name + '.conf'
-    if os.path.exists(config_file):
-      config = configparser.ConfigParser()
-      config.read(config_file)
-      configuration[config_name] = config
-    else:
-      configuration[config_name] = {}
+    for config_name in {'general', 'interfaces', 'connections', 'wireless_connections'}:
+      config_file = config_dir + config_name + '.conf'
+      if os.path.exists(config_file):
+        config = configparser.ConfigParser()
+        config.read(config_file)
+        configuration[config_name] = config
+      else:
+        configuration[config_name] = {}
 
-  dhcp_service = DHCP()
-  ip_service = IP()
-  wpa_supplicant = WpaSupplicant()
+    dhcp_service = DHCP()
+    ip_service = IP()
+    wpa_supplicant = WpaSupplicant()
 
-  interfaces = {}
+    interfaces = {}
+    wireless_interfaces = {}
 
-  for x in configuration['interfaces']:
-    if x['wireless'] == "False":
-      interface = Interface(x['name'], dhcp_service, ip_service)
-    else:
-      interface = WirelessInterface(x['name'], wpa_supplicant)
+    for x in configuration['interfaces']:
+      if x['wireless'] == "False":
+        interface = Interface(x['name'], dhcp_service, ip_service)
+      else:
+        interface = WirelessInterface(x['name'], wpa_supplicant)
 
-    interfaces[x['name']] = interface
+      interfaces[x['name']] = interface
+      
+
+    connections = {}
+    wireless_connections = {}
+
+    for x in configuration['connections']:
+      connection = Connection(x['name'], x['priority'], x['local'])
+      connections[x['name']]
+
+    for x in configuration['wireless_connections']:
+      connection = Connection(x['name'], x['priority'],
+          x['enc_mode'], x['ssid'], x['psk'], x['local'])
+      wireless_connections[x['name']] = connection
+
+    for x in interfaces:
+        for y in connections:
+          x.add_connection(y)
+
+    for x in wireless_interfaces:
+        for y in wireless_connections:
+          x.add_connection(y)
     
+    receive_queue = Queue.Queue()
+    send_queue = Queue.Queue()
 
-  connections = {}
-  wireless_connections = {}
+    client = ModenetGUI(send_queue, receive_queue)
+    client.run()
 
-  for x in configuration['connections']:
-    connection = Connection(x['name'], x['priority'], x['local'])
-    connections[x['name']]
+    while True:
+      item = receive_queue.get()
+      handle_msg(item)
 
-  for x in configuration['wireless_connections']:
-    connection = Connection(x['name'], x['priority'],
-        x['enc_mode'], x['ssid'], x['psk'], x['local'])
-    wireless_connections[x['name']] = connection
-
-  for x in interfaces:
-    if isinstance(x, WirelessInterface):
-      for y in wireless_connections:
-        x.add_connection(y)
-    else:
-      for y in connections:
-        x.add_connection(y)
-
-  dbus_receiver = Dbus_handler()
-
-  # dbus_receiver.network_selected_add_receiver(
+    # dbus_receiver.network_selected_add_receiver(
